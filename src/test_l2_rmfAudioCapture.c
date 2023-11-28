27f819f0011e18ec986dafe62acf70016c8eee60
harikrishna batthala <harikrishna.batthala@sky.uk>
2023-11-28 09:54:40 +0000

    Updated license header for skeleton and halspec name

--- src/test_l2_rmfAudioCapture.c
+++ src/test_l2_rmfAudioCapture.c
@@ -49,7 +49,7 @@
  * **Pre-Conditions:**  None@n
  * **Dependencies:** None@n
  *
- * Refer to API Definition specification documentation : [halSpec.md](../../docs/pages/halSpec.md)
+ * Refer to API Definition specification documentation : [rmf-audio-capture_halSpec.md](../../docs/pages/rmf-audio-capture_halSpec.md)
  *
  * @endparblock
  *
